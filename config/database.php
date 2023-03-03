44d2b2b503290f4344dd9a1cf8d0fe24313c04eb
PigFish <PigFish3@hotmail.com>
2023-03-03 11:22:34 +0100

    iets vergete

--- config/database.php
+++ config/database.php
@@ -3,6 +3,7 @@ define("DB_HOST", "localhost");
 define("DB_NAME", "gc_festival");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
+
 define("DB_HOSTcPANEL", "ngarritsen.gc-webhosting.nl:3306");
 define("DB_NAMEcPANEL", "ngarritsen_gc_festival");
 define("DB_USERcPANEL", "testDB_user");
